4286871df36f6da021d4916f14e35a5f135871b6
Elias Alves <elias.ti.alves@outlook.com>
2019-10-27 09:24:50 -0300

    Projeto Digital Criado

---
+++ src/main/java/com/alves/ProjetoDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.alves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDigitalApplication.class, args);
+	}
+
+}
