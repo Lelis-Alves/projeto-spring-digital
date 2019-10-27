409983fb792c24ac9b723d180498b378719a65be
Elias Alves <elias.ti.alves@outlook.com>
2019-10-27 20:06:08 -0300

    Ajuste na classe principal

--- src/main/java/com/alves/config/DataConfigurationPostgreSQL.java
+++ src/main/java/com/alves/config/DataConfigurationPostgreSQL.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-//@Profile("prod")
+@Profile("prod")
 public class DataConfigurationPostgreSQL {
 
 	@Bean
